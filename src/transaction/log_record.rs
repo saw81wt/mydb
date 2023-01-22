c44aa26ac2e03ad93a5afc1609f9d6aa03446e95
Sotaro Maehara <s.maehara81@icloud.com>
2023-01-22 18:03:00 +0900

    refactor

--- src/transaction/log_record.rs
+++ src/transaction/log_record.rs
@@ -8,8 +8,8 @@ use crate::{
 #[derive(Debug, Clone, Copy)]
 enum LogRecordType {
     CheckPoint = 0,
-    StartRecord,
-    CommitRecord,
+    Start,
+    Commit,
     Rollback,
     SetInt,
     SetString,
@@ -18,7 +18,8 @@ enum LogRecordType {
 impl From<i32> for LogRecordType {
     fn from(v: i32) -> Self {
         match v {
-            2 => LogRecordType::CommitRecord,
+            0 => LogRecordType::CheckPoint,
+            2 => LogRecordType::Commit,
             5 => LogRecordType::SetString,
             _ => todo!(),
         }
@@ -28,7 +29,8 @@ impl From<i32> for LogRecordType {
 impl From<LogRecordType> for i32 {
     fn from(l: LogRecordType) -> Self {
         match l {
-            LogRecordType::CommitRecord => 3,
+            LogRecordType::CheckPoint => 0,
+            LogRecordType::Commit => 3,
             LogRecordType::SetString => 5,
             _ => 0,
         }
@@ -36,18 +38,25 @@ impl From<LogRecordType> for i32 {
 }
 
 pub enum LogRecord {
-    CheckPoint,
-    StartRecord,
-    Commit(CommitRecord),
+    CheckPoint(TransactionRecord),
+    Start,
+    Commit(TransactionRecord),
     Rollback,
     SetInt,
     SetString(SetStringRecord),
 }
 
 impl LogRecord {
+    pub fn create_checkpoint_record(txnum: i32) -> Self {
+        LogRecord::CheckPoint(TransactionRecord {
+            record_type: LogRecordType::CheckPoint,
+            txnum,
+        })
+    }
+
     pub fn create_commit_record(txnum: i32) -> Self {
-        LogRecord::Commit(CommitRecord {
-            record_type: LogRecordType::CommitRecord,
+        LogRecord::Commit(TransactionRecord {
+            record_type: LogRecordType::Commit,
             txnum,
         })
     }
@@ -68,6 +77,11 @@ impl LogRecord {
     }
 }
 
+struct TransactionRecord {
+    record_type: LogRecordType,
+    txnum: i32,
+}
+
 struct SetStringRecord {
     record_type: LogRecordType,
     txnum: i32,
@@ -76,16 +90,17 @@ struct SetStringRecord {
     block_id: BlockId,
 }
 
-struct CommitRecord {
-    record_type: LogRecordType,
-    txnum: i32,
-}
-
 impl From<&mut Page> for LogRecord {
     fn from(page: &mut Page) -> Self {
         let record_type: LogRecordType = page.get_int(0).unwrap().into();
         match record_type {
-            LogRecordType::CommitRecord => {
+            LogRecordType::CheckPoint => {
+                let tpos = INTGER_BYTES;
+                let txnum = page.get_int(tpos).unwrap();
+
+                LogRecord::create_checkpoint_record(txnum)
+            }
+            LogRecordType::Commit => {
                 let tpos = INTGER_BYTES;
                 let txnum = page.get_int(tpos).unwrap();
 
@@ -127,15 +142,15 @@ impl From<&mut Page> for LogRecord {
 impl LogRecord {
     pub fn write_to_log(&self, log_manager: Arc<Mutex<LogManager>>) -> i32 {
         match self {
-            Self::Commit(commit) => {
+            Self::CheckPoint(record) | Self::Commit(record) => {
                 let tpos = INTGER_BYTES;
                 let record_len = tpos + INTGER_BYTES;
 
                 let buf = Vec::with_capacity(record_len);
                 let mut page = Page::from(Box::from(buf));
 
-                page.set_int(0, LogRecordType::CommitRecord.into()).unwrap();
-                page.set_int(tpos, commit.txnum).unwrap();
+                page.set_int(0, record.record_type.into()).unwrap();
+                page.set_int(tpos, record.txnum).unwrap();
 
                 log_manager
                     .lock()
